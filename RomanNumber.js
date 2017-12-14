18a14b065bd44e46c2df3b3d67abbe401f0f548a
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2017-12-14 12:00:45 +0200

    validation of arabics

--- RomanNumber.js
+++ RomanNumber.js
@@ -2,6 +2,8 @@ export default class RomanNumber {
 
   static errorTypes = {
     VALUE_REQUIRED: 'VALUE_REQUIRED',
+    NUMBER_NOT_INTEGER: 'NUMBER_NOT_INTEGER',
+    OUTSIDE_RANGE: 'OUTSIDE_RANGE',
   };
 
   valueKey = Symbol();
@@ -14,6 +16,20 @@ export default class RomanNumber {
     if (typeof value === 'undefined' || value === null || value === '') {
       this[this.valueKey] = null;
       throw new Error(RomanNumber.errorTypes.VALUE_REQUIRED);
+    }
+
+    if (typeof(value === 'number')) {
+      RomanNumber.validateArabicNumber(value);
+      this[this.valueKey] = value;
+    }
+  }
+
+  static validateArabicNumber(value) {
+    if (!Number.isInteger(value)) {
+      throw new Error(RomanNumber.errorTypes.NUMBER_NOT_INTEGER);
+    }
+    if (value < 1 || value > 3999) {
+      throw new Error(RomanNumber.errorTypes.OUTSIDE_RANGE);
     }
   }
 

14d40a258b06ca04ad903f830a9df19baceb3b20
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2017-12-14 11:27:35 +0200

    class getter and setter for the input value

--- RomanNumber.js
+++ RomanNumber.js
@@ -1,8 +1,25 @@
 export default class RomanNumber {
 
   static errorTypes = {
-    VALUE_REQUIRED: 'valueRequired',
+    VALUE_REQUIRED: 'VALUE_REQUIRED',
   };
+
+  valueKey = Symbol();
+
+  constructor(value) {
+    this.value = value;
+  }
+
+  set value(value) {
+    if (typeof value === 'undefined' || value === null || value === '') {
+      this[this.valueKey] = null;
+      throw new Error(RomanNumber.errorTypes.VALUE_REQUIRED);
+    }
+  }
+
+  get value() {
+    return this[this.valueKey];
+  }
 
   toString() {
 
